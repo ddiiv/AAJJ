6c41d8152d596cf8aee542139247ae30b4c12887
dantolin45 <dantolin45@gmail.com>
2023-07-10 00:27:48 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import MainMainComponent from './components/main/MainMainComponent';
+
+
+function App() {
+  return (
+    <div className="App">
+      <MainMainComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/main/MainMainComponent.js
@@ -0,0 +1,8 @@
+import React,{useState, useEffect} from "react";
+
+const MainMainComponent = () => {
+
+
+
+}
+export default MainMainComponent;
