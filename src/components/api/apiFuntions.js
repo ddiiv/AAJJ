be2607af7e513e3d94fa55307708afe7d03ec849
dantolin45 <dantolin45@gmail.com>
2023-07-11 12:08:37 -0300

    a laburar

--- src/components/api/apiFuntions.js
+++ src/components/api/apiFuntions.js
@@ -1,2 +1,9 @@
 import React from "react";
 import axios from "axios";
+
+const getImages=()=>{
+    /*hacer get img para carrusel*/
+
+
+
+}
--- src/components/main/MainCarrusel.jsx
+++ src/components/main/MainCarrusel.jsx
@@ -2,6 +2,11 @@ import React,{useState,useEffect} from 'react';
 
 const MainCarrusel = () => {
     const [carrusel, setCarrusel] = useState([]);
+    
+    
+    
+    
+    
     return (
         <div>
             <h1>hola</h1>
