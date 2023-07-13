3ae82575d97a70a13165c12d884674098333392b
dantolin45 <dantolin45@gmail.com>
2023-07-13 00:05:48 -0300

    --------

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-
 import './App.css';
 import MainMainComponent from './components/main/MainMainComponent';
-import FutbolCatalog from './components/futbol/Futbol';
+import search from'./img/search.png'
+import user from'./img/user.png'
 
 
 function App() {
   return (
-    <div className="App">
-      <MainMainComponent/>
-      <FutbolCatalog/>
-    </div>
+
+  <body>
+  <ul class="topnav">
+  <li><a href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about"><img className="img" src={search}/></a></li>
+  <li class="right"><a href="#about"><img className="img" src={user}/></a></li>
+  
+ 
+</ul>
+
+<div className='main'>
+  <h2>Responsive Topnav Example</h2>
+  <p><MainMainComponent/></p>
+  <p><MainMainComponent/></p>  <p><MainMainComponent/></p>  <p><MainMainComponent/></p>  <p><MainMainComponent/></p>  <p><MainMainComponent/></p>  <p><MainMainComponent/></p>  <p><MainMainComponent/></p>  <p><MainMainComponent/></p>  <p><MainMainComponent/></p>  <p><MainMainComponent/></p>  <p><MainMainComponent/></p>  <p><MainMainComponent/></p>
+</div>
+
+            {/* <FutbolCatalog/> */}
+        
+  </body>
+
   );
 }
 
