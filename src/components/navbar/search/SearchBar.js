2f15216011e15abe2762f69ef951743d0a199d51
dantolin45 <dantolin45@gmail.com>
2023-09-22 09:53:21 -0300

    arreglo context

--- src/components/navbar/search/SearchBar.js
+++ src/components/navbar/search/SearchBar.js
@@ -2,19 +2,19 @@ import React, { useState, useRef, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import search_png from "../../../img/search.png";
 import '../../../css/NavBar.css';
-import {getProducts} from '../../../api/apiFuntions.js';
-import {SearchContext}  from "../../../context/SearchContext.js";
- 
-const SearchBar = () =>{
+import { getProducts } from '../../../api/apiFuntions.js';
+import { SearchContext } from "../../../context/SearchContext.js";
+
+const SearchBar = () => {
     const [search, setSearch] = useState('');
     const handleChange = e => setSearch(e.target.value);
     const searchInput = useRef()
-    const {searchProducts, setSearchProducts} = useContext(SearchContext);
+    const { searchProducts, setSearchProducts } = useContext(SearchContext);
 
-    useEffect(() => setSearchProducts(getProducts()),[])
+    useEffect(() => setSearchProducts(getProducts()), [])
     console.log("searchpr" + searchProducts)
     useEffect(() => {
-    },[])
+    }, [])
 
     useEffect(() => { // Filtrar
         let list = [...searchProducts]
@@ -26,15 +26,15 @@ const SearchBar = () =>{
         }
         setSearchProducts(list);
     }, [search])
-    
-    return(
+
+    return (
         <div className="navItem" id="searchBar">
             <input placeholder="Buscar"
-                   className="navItem"
-                   value={search}
-                   onChange={handleChange}
-                   ref={searchInput} />
-            <Link to={`/search` }><img className="items" src={search_png}/></Link>
+                className="navItem"
+                value={search}
+                onChange={handleChange}
+                ref={searchInput} />
+            <Link to={`/search`}><img className="items" src={search_png} /></Link>
         </div>
     )
 }
--- src/context/SearchContext.js
+++ src/context/SearchContext.js
@@ -1,11 +1,11 @@
-import React , { createContext, useState }from 'react';
+import React , { createContext, useState }from "react";
 
 const SearchContext = createContext();
 
-const SearchProvider = ({props}) => {
+const SearchProvider = ({children}) => {
     const [searchProducts, setSearchProducts] = useState([]);
 
-    return <SearchContext.Provider value={{searchProducts, setSearchProducts}}>{props.children}</SearchContext.Provider>
+    return <SearchContext.Provider value={{searchProducts, setSearchProducts}}>{children}</SearchContext.Provider>
 }
 
 export {SearchProvider, SearchContext};
