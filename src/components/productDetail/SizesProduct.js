6bbcdb6d492397f3a44871151cfb8032d40974c6
dantolin45 <dantolin45@gmail.com>
2023-09-25 00:17:37 -0300

    sizexproductdetail terminado

--- src/components/productDetail/SizesProduct.js
+++ src/components/productDetail/SizesProduct.js
@@ -5,9 +5,10 @@ import StockProductxSize from "./StockProductxSize";
 const SizesProduct = ({ idProductS })   => {
 
     const idProductSelected = idProductS;
-
+    const [StockxSize, setStockxSize] = useState([]);
+    const [frameWork, setFrameWork] = useState([]);
     const [sizesProduct, setSizesProduct] =  useState([]);
-    const [stock, setStock] = useState(false);
+    const [showStock, setShowStock] = useState(false);
     useEffect(()=>{
         getSizesByIdProduct(idProductSelected).then((data)=>{
 
@@ -15,14 +16,17 @@ const SizesProduct = ({ idProductS })   => {
         })
 
     }, [])
-    const handleStock = () => {
-            setStock(true)
-    
+    const handleStock = (e) => {
+            const a = e.target.value;
+            setFrameWork(e.target.value);
+            setShowStock(true);
+            setStockxSize(sizesProduct[a-1]);
+
     }
 
     return (
         <>
-            
+                <div className="SizeItems">
                 {sizesProduct.map((size) =>{
                 if(size.Quantity === 0){
                     return(
@@ -37,11 +41,13 @@ const SizesProduct = ({ idProductS })   => {
                     
                 return(
                     <div className="SizeItems">
-                        <input type="radio" className="itemSize" value={size.size} specification={size.size} onClick={handleStock}></input><b className="sizeCircle" key={size.IdSize} for={size.Size}>{size.size}</b> 
+                        <input type="radio" className="itemSize" name="itemSize" value={size.IdSize} specification={size.size} onChange={handleStock} checked={frameWork === size.IdSize ? true : false}></input><b className="sizeCircle" key={size.IdSize} for={size.Size}>{size.size}</b> 
                     </div>
                 )}})}
-                {stock ? <StockProductxSize Size={sizesProduct} /> : null}
-
+                 </div>   
+                <div className="StockXProductSizxSize">
+                    {showStock ? <StockProductxSize Size={StockxSize} /> : null}
+                </div>   
             
         </>
     )
--- src/components/productDetail/StockProductxSize.js
+++ src/components/productDetail/StockProductxSize.js
@@ -1,15 +1,44 @@
 import React,{useState} from "react";
 
 const StockProductxSize = ({ Size })   => {
-    
-        const [stock, setStock] = useState([]);
+    const [stock, setStock] = useState(1);
+    const handleStock = (e) => {
+        console.log(e.target.value)
+        setStock(e.target.value);
+    }
+    const handleStockAdd = (e) => {
+        console.log(e.target.value)
+        if(stock < Size.Quantity)
+        {
+        setStock(stock + 1);
+        }
+        else{
+            alert("No hay mas stock disponible")
+        }
+    }
+    const handleStockSubs = (e) => {
+        console.log(e.target.value)
+        if(stock > 1)
+        {
+        setStock(stock - 1);
+        }
+        else{
+            alert("No se puede seleccionar menos de 1")
+        }
+    }
 
-    
+
         return (
             <>
                 <div className="StockProductxSize">
-                    <h3 className="StockProductxSizeTitle">Selecciono el talle: {Size[0].size}</h3>
-                    <input className="StockProductxSizeText"></input>
+                    <h3 className="StockProductxSizeTitle">Seleccionó el talle: {Size.size}</h3>
+                    <div className="subs">
+                        <button className="subsButton" onClick={handleStockSubs}>-</button>
+                    </div>
+                    <input type="number" value={stock}  min={1} max={Size.Quantity}  className="StockProductxSizeNumber" onChange={handleStock} disabled ></input>
+                    <div className="adds">
+                    <button className="subsButton" onClick={handleStockAdd}>+</button>
+                    </div>
                 </div>
 
     
