8e56af3a077e5bf5027dabc269e0d5f40e5175ca
dantolin45 <dantolin45@gmail.com>
2023-12-18 13:38:15 -0300

    update app

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import cors from 'cors'
+import allRoutes from './routes/allRoutes.js'
+
+const app = express()
+app.set('port', process.env.PORT || 3001)
+
+app.use(cors())
+app.use(express.json())
+
+allRoutes.forEach(router => app.use(router))
+
+
+export default app
