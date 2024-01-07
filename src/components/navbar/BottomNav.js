e370f8e6c4de6087f13db9d3a8fa43df2a10fb29
dantolin45 <dantolin45@gmail.com>
2024-01-07 17:46:55 -0300

    v.0.0.11

--- src/components/navbar/BottomNav.js
+++ src/components/navbar/BottomNav.js
@@ -1,13 +1,43 @@
 import { BrowserRouter as Routes, Link } from "react-router-dom";
 import '../../css/NavBar.css'
+import search_png from "../../img/search.png"
+import { useSearchFunctions } from "../../context/SearchContext";
+import { useNavigate } from "react-router-dom";
 
-const BottomNav = ({category}) => {
+
+const BottomNav = ({ category }) => {
+    const navigate = useNavigate();
+    const contextFunctions = useSearchFunctions();
+  
+    const redurectSubmit =(event)=>{
+       event.preventDefault();
+       navigate(`/search=?${contextFunctions.searchInput}`);
+    }
+
     return (
-        <div className='nav'>
-            {category?.map((categories) => {
-                return <li className='nav-item' key={categories.IdCategory} ><Link to={`/category/${categories.Category}`} id="a" className='navItem'>{categories.Category}</Link></li>
-            })}
+        <div className='nav-bottom_categories'>
+            <div className="categories_container enabled">
+                {category?.map((categories) => {
+                    return <li className='nav-item' key={categories.IdCategory} ><Link to={`/category/${categories.Category}`} id="a" className='navItem'>{categories.Category}</Link></li>
+                })}
+            </div>
+            <div className="serachbox_cotainer disabled">
+                <div className="navItem disabled" id="searchBar">
+                    <form className="serachBar__Form-Container" onSubmit={redurectSubmit}>
+                        <button className='buttonItem' type="submit" value={contextFunctions.searchInput} onClick={contextFunctions.handleSearchProductsByInput} >
+                            <Link to={`/search=?${contextFunctions.searchInput}`} className="nothing"><img className="items" src={search_png} alt="" /></Link>
+                        </button>
+                        <input
+                            type="text"
+                            placeholder="Buscar"
+                            className="searchInput"
+                            onChange={contextFunctions.handleChangeSearch}
+                        />
 
+                        
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/navbar/TopNav.js
+++ src/components/navbar/TopNav.js
@@ -24,9 +24,14 @@ const TopNav = ({ category }) => {
                         <div className='responsive_logo disabled'>
                             <button className='hamburguer-logo__button' onClick={() => setShow(!show)}
                             >
-                                <img className='logo' src={bichologo} alt="" />
+                                <svg class="svg-icon-aside" viewBox="0 0 20 20">
+                                    <path d="M3.314,4.8h13.372c0.41,0,0.743-0.333,0.743-0.743c0-0.41-0.333-0.743-0.743-0.743H3.314
+								c-0.41,0-0.743,0.333-0.743,0.743C2.571,4.467,2.904,4.8,3.314,4.8z M16.686,15.2H3.314c-0.41,0-0.743,0.333-0.743,0.743
+								s0.333,0.743,0.743,0.743h13.372c0.41,0,0.743-0.333,0.743-0.743S17.096,15.2,16.686,15.2z M16.686,9.257H3.314
+								c-0.41,0-0.743,0.333-0.743,0.743s0.333,0.743,0.743,0.743h13.372c0.41,0,0.743-0.333,0.743-0.743S17.096,9.257,16.686,9.257z"></path>
+                                </svg>
                             </button>
-
+                            <Link to="/" className='nothing-aside-logo' ><img className='logo' src={bichologo} alt="" /></Link>
                             <div className='modal-background' id={showChangueCss()} onClick={() => setShow(false)}>
 
                                 <aside className='aside_nav--left' id={showChangueCss()}>
